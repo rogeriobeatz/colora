b9311d1a97986ccf4e9e87ec4cc4ce82a6460508
[dyad] <rogerio_silva93@yahoo.com.br>
2026-02-20 20:49:30 -0300

    Updated src/pages/Landing.tsx

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -14,7 +14,7 @@ const Landing = () => {
           <Link to="/" className="flex items-center gap-2">
 
 
-            <img src={logoSvg} alt="Logotipo Colora" className="w-34 "/>
+            <img src={logoSvg} alt="Logotipo Colora" className="w-32 "/>
           </Link>
           <div className="flex items-center gap-3">
             <Button variant="ghost" asChild>
